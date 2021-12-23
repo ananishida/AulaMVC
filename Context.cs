08c099d300f37bb47bd9218b6543b4f5c2189cbf
ananishida <ananishida@outlook.com>
2021-12-22 22:10:47 -0300

    Adicionar arquivos de projeto.

---
+++ Context.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC221202021.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC221202021
+{
+    public class Context : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Email> Emails { get; set; }
+        public Context()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost; initial Catalog=AulaEntity; User ID=usuario; password=senha; language= Portuguese;Trusted_Connection=True");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Email>()
+                .HasOne(e => e.pessoa)
+                .WithMany(p => p.Emails)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MVC221202021.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC221202021.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
+        public IActionResult Pessoas()
+        {
+            Context context = new Context();
+            List<Pessoa> pessoas = (from Pessoa p in context.Pessoas select p).Include(e => e.Emails).ToList<Pessoa>();
+            return View(pessoas);
+        }
+
+        public IActionResult PessoaID(int id)
+        {
+            Context context = new Context();
+            Pessoa? p = context.Pessoas.Find(id);
+            return View(p);
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Pessoa p)
+        {
+            Context context = new Context();
+            context.Pessoas.Add(p);
+            context.SaveChanges();
+
+            return RedirectToAction("PessoaID", new { id = p.Id});
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
